37b3c9f3226391290006f14c74f2e6f655f2e9a5
Justin Hu <juphu@ucsc.edu>
2022-04-18 16:06:01 -0700

    comments

--- src/main.js
+++ src/main.js
@@ -12,3 +12,18 @@ let borderUISize = game.config.height / 15;
 let borderPadding  = borderUISize / 3;
 
 let keyF, keyR, keyLEFT, keyRIGHT, mouseClick;
+
+/**
+ * Justin Hu
+ * RocketPatrolMod
+ * 4/18/22
+ * ~ 5 hours to complete
+ * 
+ * 30 points - Simultaneous 2 Player Mode
+ * 20 points - Mouse Control
+ * 20 points - New Spaceship Type
+ * 20 points - Timer system that gets added to on hit
+ * 10 points - Visible Timer
+ * 
+ * 
+ */
